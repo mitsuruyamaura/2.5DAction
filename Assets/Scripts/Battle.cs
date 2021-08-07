7e16eef8e9b535fdb85704bc6125bf77e826acf1
mitsuru_yamaura <komayou326@gmail.com>
2021-08-08 01:36:15 +0900

    SceneStateManager.cs に、シーン遷移時のフェードイン/フェードアウト機能の実装。シーンの読み込みが終了してからフェードアウトする制御を実装。 それに伴い、Battle.cs 、EnemySymbol.cs の処理を修正。デバッグ開始。

--- Assets/Scripts/Battle.cs
+++ Assets/Scripts/Battle.cs
@@ -38,6 +38,7 @@ public class Battle : MonoBehaviour
     [SerializeField]
     private PlayerController playerController;
 
+
     IEnumerator Start()
     {
         SceneStateManager.instance.battle = this;
@@ -46,7 +47,7 @@ public class Battle : MonoBehaviour
             yield return new WaitForSeconds(1.0f);
 
             // Debug用
-            SceneStateManager.instance.PreparateNextScene(SceneName.Stage);
+            SceneStateManager.instance.PreparateStageScene();
         } else {
             // バトル開始時の処理
             StartCoroutine(OnEnter());
@@ -90,8 +91,6 @@ public class Battle : MonoBehaviour
 
 
         Debug.Log("バトル開始");
-
-        yield return null;
     }
 
     /// <summary>
@@ -120,14 +119,12 @@ public class Battle : MonoBehaviour
     /// <returns></returns>
     public IEnumerator OnExit() {
 
-
         Debug.Log("バトル終了時の処理");
-
 
         // TODO 終了時の処理
         // リザルト表示  表示内で new WaitUntil や UniRX で監視して画面のタップを待つ
 
-        // Debug
+        // Battle 終了の余韻
         yield return new WaitForSeconds(1.0f);
 
         // ノーダメージボーナスの判定
@@ -142,14 +139,9 @@ public class Battle : MonoBehaviour
 
         Debug.Log("バトル終了");
 
-        yield return null;
-
-
-        // トランジション処理
 
         // Stage へ戻る
-        SceneStateManager.instance.PreparateNextScene(SceneName.Stage);
-
+        SceneStateManager.instance.PreparateStageScene();
     }
 
     /// <summary>
