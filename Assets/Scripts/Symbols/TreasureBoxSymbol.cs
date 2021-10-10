dc99753632bdde7599b4515b3d00f39ecebe4056
mitsuru_yamaura <komayou326@gmail.com>
2021-10-10 19:31:54 +0900

    アイテムのデータに重み付けを追加。トレジャーボックスシンボルに侵入した際に、ドロップするアイテムのリストからランダムなアイテムを１つ排出する機能を実装。 レアリティによる加味はなし。今後追加する。シンボルのアニメ演出追加。取得時のアニメ演出はまだ未実装(現在はオーブと同じ演出にしている)。 デバッグ開始。

--- Assets/Scripts/Symbols/TreasureBoxSymbol.cs
+++ Assets/Scripts/Symbols/TreasureBoxSymbol.cs
@@ -2,27 +2,55 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using DG.Tweening;
+using System.Linq;
 
 public class TreasureBoxSymbol : SymbolBase
 {
-    public int treasureLevel;
+    public override void OnEnterSymbol(SymbolManager symbolManager) {
+        base.OnEnterSymbol(symbolManager);
 
+        tween = transform.DOPunchScale(Vector3.one * 0.35f, 3.0f, 1)
+            .SetEase(Ease.OutBack)
+            .SetLoops(-1, LoopType.Restart);
+    }
 
     public override void TriggerAppearEffect(MapMoveController mapMoveController) {
+
         base.TriggerAppearEffect(mapMoveController);
 
-        // 抽出
+        // 抽選用の重みの合計値を計算
+        int totalWeight = DataBaseManager.instance.dropItemDatasList.Select(x => x.weight).Sum();
 
+        //Debug.Log(totalWeight);
 
-        // 抽選
+        // ランダムな値を取得
+        int randaomValue = Random.Range(0, totalWeight);
 
+        //Debug.Log(randaomValue);
 
-        // TODO 演出
+        // 抽選結果のアイテムの保持よう
+        AbilityItemDataSO.AbilityItemData getItemData = null;
 
+        // 抽選(今回はレアリティも混合で Weight のみで抽選)
+        foreach (AbilityItemDataSO.AbilityItemData itemData in DataBaseManager.instance.dropItemDatasList) {
+            if (randaomValue <= itemData.weight) {
+                getItemData = itemData;
+                break;
+            }
+            randaomValue -= itemData.weight;
+        }
+
+        // TODO 演出(現在はオーブと同じもの)
+        GameObject effect = Instantiate(EffectManager.instance.orbGetEffectPrefab, effectTran);
+        effect.transform.SetParent(EffectManager.instance.effectConteinerTran);
+
+        Destroy(effect, 1.5f);
 
         // GameData に追加
+        GameData.instance.AddaAbilityItemDatasList(getItemData.abilityType, getItemData.abitilyNo);
 
-
+        // 破棄
+        base.OnExitSymbol();
     }
 
 
